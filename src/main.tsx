cd8952743f5c0f8b56e0885df942e4a6827ae164
Ricardo Trentin <rtrentin@aviatrix.com>
2026-05-05 13:45:20 -0400

    chore: update Sentry config with browser tracing and performance monitoring

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,9 @@ import { ThemeProvider } from './lib/ThemeContext'
 Sentry.init({
   dsn: 'https://41bfd7e597c2524b769374972e269ee1@o4511332226891776.ingest.us.sentry.io/4511338342580224',
   sendDefaultPii: true,
+  integrations: [Sentry.browserTracingIntegration()],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ['localhost', /^https:\/\/yourserver\.io\/api/],
 })
 
 createRoot(document.getElementById('root')!).render(
